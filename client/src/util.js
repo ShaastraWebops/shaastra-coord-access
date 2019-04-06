0450908197225f974f5a4a261c1323998d347631
Yash Gupta <gyash24x7@gmail.com>
2019-04-06 21:55:32 +0530

    added build v2

--- client/src/util.js
+++ client/src/util.js
@@ -1,7 +1,6 @@
 import { Dropbox } from "dropbox";
 import fetch from "isomorphic-fetch";
 
-console.log(process.env.REACT_APP_DROPBOX_ACCESS_TOKEN);
 const accessToken = process.env.REACT_APP_DROPBOX_ACCESS_TOKEN;
 
 const dbx = new Dropbox({ accessToken, fetch });
